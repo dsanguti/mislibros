faf919c23deead8446c85e2495b05185bcf3c02e
dsanguti <dsanguti@gmail.com>
2024-10-29 10:58:55 +0100

    Navlink en titulo y subtitulo para ir a inicio

--- src/assets/components/Tittle.jsx
+++ src/assets/components/Tittle.jsx
@@ -1,16 +1,24 @@
 import PropTypes from "prop-types"; // Importa PropTypes
+import { NavLink } from "react-router-dom";
 import style from "../css/tittle.module.css";
-
 
 const Tittle = ({ children }) => {
   return (
+    <NavLink className={style.a} to="/">
     <div className={style.container}>
-      <div className={style.containertittle}>
-        <img className={style.logo} src="../../../public/logoAppColor.png" alt="logo de App" />
-        <h1 className={style.tittle}>{children}</h1>
-      </div>
-      <h3 className={style.subtittle}> Tu biblioteca siempre a mano</h3>
+ 
+        <div className={style.containertittle}>
+          <img
+            className={style.logo}
+            src="../../../public/logoAppColor.png"
+            alt="logo de App"
+          />
+          <h1 className={style.tittle}>{children}</h1>
+        </div>
+        <h3 className={style.subtittle}> Tu biblioteca siempre a mano</h3>
+     
     </div>
+    </NavLink>
   );
 };
 
